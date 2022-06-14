e890bbb8763d548566bfbefb1dc64d5679a5d2b6
kovacsv <viktorkovacs@gmail.com>
2022-06-14 19:56:08 +0200

    Remove o3dv binaries.

--- use_as_browser_package/o3dv/libs/loaders/occt-import-js-worker.js
+++
@@ -1,8 +0,0 @@
-importScripts ('occt-import-js.js');
-
-onmessage = async function (ev)
-{
-	let occt = await occtimportjs ();
-	let result = occt.ReadStepFile (ev.data);
-	postMessage (result);
-};
